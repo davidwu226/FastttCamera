87e95f1c7e4ebd141ec72db2bb5bee0777032209
David Wu <davidwu@italia-2.local>
2015-06-24 12:07:24 -0700

    Created Framework target.

---
+++ FastttCamera/FastttCameraFramework.h
@@ -0,0 +1,31 @@
+//
+//  FastttCameraFramework.h
+//  FastttCamera
+//
+//  Created by David Wu on 6/24/15.
+//  Copyright (c) 2015 Food Research. All rights reserved.
+//
+
+#ifndef FastttCamera_FastttCameraFramework_h
+#define FastttCamera_FastttCameraFramework_h
+
+// Base
+#import <FastttCamera/FastttCamera.h>
+#import <FastttCamera/AVCaptureDevice+FastttCamera.h>
+#import <FastttCamera/FastttCameraInterface.h>
+#import <FastttCamera/FastttCameraTypes.h>
+#import <FastttCamera/FastttCapturedImage.h>
+#import <FastttCamera/FastttCapturedImage+Process.h>
+#import <FastttCamera/FastttFocus.h>
+#import <FastttCamera/IFTTTDeviceOrientation.h>
+#import <FastttCamera/UIImage+FastttCamera.h>
+#import <FastttCamera/UIViewController+FastttCamera.h>
+
+// Filters
+#import <FastttCamera/FastttEmptyFilter.h>
+#import <FastttCamera/FastttFilter.h>
+#import <FastttCamera/FastttFilterCamera.h>
+#import <FastttCamera/FastttLookupFilter.h>
+#import <FastttCamera/UIImage+FastttFilters.h>
+
+#endif
